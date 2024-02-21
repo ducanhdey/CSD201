4931457067789a1dc9f9f0ef3a3f265dbd950d77
Phan Duc anh <114552003+ducanhdey@users.noreply.github.com>
2024-02-22 06:38:02 +0700

    delete the temp file of doc

--- src/main/java/org/example/StackandQueue/Problem/Prob20.java
+++ src/main/java/org/example/StackandQueue/Problem/Prob20.java
@@ -1,2 +1,38 @@
-package org.example.StackandQueue.Problem;public class Prob20 {
+package org.example.StackandQueue.Problem;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Prob20 {
+	public static boolean isValid(String s) {
+
+		Stack<Character> myStack = new Stack<Character>();
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (c == '(' || c == '{' || c == '[') {
+				myStack.push(c);
+			} else {
+				if(myStack.isEmpty()) {
+					return false;
+				}
+				char openPeek = myStack.peek();
+				if (c == ')' && openPeek == '('
+						|| c == '}' && openPeek == '{'
+						|| c == ']' && openPeek == '[') {
+					myStack.pop();
+
+				} else {
+					return false;
+				}
+			}
+		}
+		return myStack.isEmpty() ? true : false;
+	}
+
+	public static void main(String[] args) {
+		String s = new Scanner(System.in).nextLine();
+		System.out.println(isValid(s));
+	}
+
 }
+
--- src/main/java/org/example/StackandQueue/UsingLibrary.java
+++ src/main/java/org/example/StackandQueue/UsingLibrary.java
@@ -1,18 +1,37 @@
 package org.example.StackandQueue;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class UsingLibrary {
 	public static void main(String[] args)  {
-		Stack<Integer> stack = new Stack<Integer>();
-		stack.push(1);
-		stack.push(2);
-		stack.push(3);
-		stack.stream().forEach(System.out::print);
-		stack.pop();
+//		Stack<Integer> stack = new Stack<Integer>();
+//		stack.push(1);
+//		stack.push(2);
+//		stack.push(3);
+//		stack.stream().forEach(System.out::print);
+//		stack.pop();
+//		System.out.println();
+//		stack.stream().forEach(System.out::print);
+//		System.out.println(stack.peek());
+
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(1);
+		queue.add(2);
+		queue.add(3);
+		queue.stream().forEach(System.out::print);
 		System.out.println();
-		stack.stream().forEach(System.out::print);
-		System.out.println(stack.peek());
+		queue.remove();
+		queue.stream().forEach(System.out::print);
+		System.out.println();
+
+		//poll
+		System.out.println(queue.poll());
+		queue.stream().forEach(System.out::print);
+		//element
+		System.out.println(queue.element());
+		queue.stream().forEach(System.out::print);
 
 	}
 }
