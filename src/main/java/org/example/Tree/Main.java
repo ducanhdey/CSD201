1ab80fb018d5e3a2a4740d48bcf7d4eeb75f1235
Phan Duc anh <114552003+ducanhdey@users.noreply.github.com>
2024-03-01 07:00:49 +0700

    tree

--- src/main/java/org/example/Tree/Main.java
+++
@@ -1,7 +0,0 @@
-package org.example.Tree;
-
-public class Main {
-	public static void main(String[] args){
-
-	}
-}
---
+++ src/main/java/org/example/Tree/UsingLinkedList/Main.java
@@ -0,0 +1,10 @@
+package org.example.Tree.UsingLinkedList;
+
+public class Main {
+	public static void main(String[] args){
+		MyBinaryTree myBinaryTree = new MyBinaryTree();
+		myBinaryTree.init();
+
+		System.out.println("Done");
+	}
+}
